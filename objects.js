c62d8075ea2a9847ad756afbaa76b9c3d6a73f31
Yu Li <yli0607x@gmail.com>
2018-08-07 01:51:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,3 @@
-var playlist = {
-  artistName: songTitle
-};
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artist]
-  return playlist
-}
+var playlist(
+  a:"b"
+)
